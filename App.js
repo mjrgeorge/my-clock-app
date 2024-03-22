02c209f34398d75a0842ce65811b6ad34f72113f
mjrgeorge <mjrgeorge@gmail.com>
2024-03-22 22:30:09 +0600

    initial setup with inter font add

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
 export default function App() {
+    let [fontsLoaded] = useFonts({
+    inter_regular: Inter_400Regular,
+    inter_bold: Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: "inter_regular", fontSize: 20}}>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: "inter_bold", fontSize: 20}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
