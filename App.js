7ffe5775ac670c6c35a190845647fe286d94eaad
mjrgeorge <mjrgeorge@gmail.com>
2024-03-22 23:00:21 +0600

    parent view and upper portion done

--- App.js
+++ App.js
@@ -1,31 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
-import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
+import {
+  ActivityIndicator,
+  ImageBackground,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+import { useState } from "react";
 
 export default function App() {
-    let [fontsLoaded] = useFonts({
-    inter_regular: Inter_400Regular,
-    inter_bold: Inter_700Bold,
+  let [fontsLoaded] = useFonts({
+    "Inter-Regular": Inter_400Regular,
+    "Inter-Bold": Inter_700Bold,
   });
+  const [showMore, setShowMore] = useState(false);
 
   if (!fontsLoaded) {
     return <ActivityIndicator />;
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={{fontFamily: "inter_regular", fontSize: 20}}>Open up App.js to start working on your app!</Text>
-      <Text style={{fontFamily: "inter_bold", fontSize: 20}}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require("./assets/light-bg.png")}
+      style={{ flex: 1 }}
+    >
+      {/* PARENT VIEW */}
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "space-between",
+          marginTop: 32,
+          paddingHorizontal: 26,
+        }}
+      >
+        {/* UPPER PORTION OF THE SCREEN */}
+        {!showMore && (
+          <View style={{ flexDirection: "row" }}>
+            <View style={{ flex: 1 }}>
+              <Text
+                style={{
+                  fontFamily: "Inter-Regular",
+                  fontSize: 12,
+                  color: "#fff",
+                }}
+              >
+                “The science of operations, as derived from mathematics more
+                especially, is a science of itself, and has its own abstract
+                truth and value.”
+              </Text>
+              <Text
+                style={{
+                  fontFamily: "Inter-Bold",
+                  color: "#fff",
+                  fontSize: 12,
+                  marginTop: 8,
+                }}
+              >
+                - Ada Lovelace
+              </Text>
+            </View>
+            <Image source={require("./assets/refresh.png")} />
+          </View>
+        )}
+      </View>
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
